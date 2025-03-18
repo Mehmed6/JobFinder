356d8c2b761226dcb4b885e0ee4e66ea0712dba3
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-03-18 06:07:46 +0100

    Security settings have been configured
    The application's security configuration has been updated to ensure that only authorized users can access specific areas. Public access has been granted for login and registration, while access to the admin panel is restricted to users with the admin role. Additionally, session management, error handling, and logout processes have been configured.

---
+++ JobFinder-v1.0/src/main/java/com/doganmehmet/app/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.doganmehmet.app.config;
+
+import com.doganmehmet.app.security.CustomAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String LOGIN = "/auth/login";
+    private final String REGISTER = "/register";
+    private final String ADMIN = "/admin/**";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests(request ->
+                request.requestMatchers(LOGIN, REGISTER).permitAll()
+                        .requestMatchers(ADMIN).hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::migrateSession)
+                        .maximumSessions(1)
+                        .expiredUrl("/auth/login?expired=true")
+                        .maxSessionsPreventsLogin(false))
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+                .logout(logout -> logout
+                        .logoutUrl("logout")
+                        .logoutSuccessUrl(LOGIN)
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID"));
+
+        return http.build();
+    }
+}
