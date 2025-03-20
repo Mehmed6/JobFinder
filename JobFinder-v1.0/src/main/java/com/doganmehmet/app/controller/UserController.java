e7aa38487430240a6e28901d1fd6892be8a932f7
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-03-20 07:14:00 +0100

    UserController added
    A UserController was created to handle HTTP requests related to user operations.
    Endpoints for user registration, updating user details, retrieving user data, and other user-related actions were implemented.
    Proper error handling and validation mechanisms were added for each endpoint.
    The controller allows interaction with the user-related data, facilitating user management functionality in the application.

---
+++ JobFinder-v1.0/src/main/java/com/doganmehmet/app/controller/UserController.java
@@ -0,0 +1,138 @@
+package com.doganmehmet.app.controller;
+
+import com.doganmehmet.app.dto.user.UpdateUserDTO;
+import com.doganmehmet.app.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService m_userService;
+
+    public UserController(UserService userService)
+    {
+        m_userService = userService;
+    }
+
+    @GetMapping("/show/{userId}")
+    public String showUser(@PathVariable long userId, Model model)
+    {
+        try {
+            model.addAttribute("user", m_userService.findUser(userId));
+        }
+        catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return "error/errorPage";
+        }
+
+        return "user/user";
+    }
+
+    @GetMapping("/update")
+    public String showUpdateUserPage(Model model)
+    {
+        model.addAttribute("updateUserDTO", new UpdateUserDTO());
+        return "user/updateUser";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@Valid UpdateUserDTO updateUserDTO, BindingResult bindingResult, Model model)
+    {
+        if (bindingResult.hasErrors())
+            return "user/updateUser";
+
+        try {
+            model.addAttribute("updateUserDTO", m_userService.updateUser(updateUserDTO));
+            model.addAttribute("message", "User updated successfully");
+        }
+        catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return "error/errorPage";
+        }
+
+        return "user/updateUser";
+    }
+
+    @GetMapping("/experience")
+    public String showUsersPage(@RequestParam(required = false, defaultValue = "0") int minExperience,
+                                @RequestParam(required = false, defaultValue = "0") int maxExperience,
+                                @RequestParam(defaultValue = "0") int page,
+                                @RequestParam(defaultValue = "5") int size, Model model)
+    {
+
+        model.addAttribute("users",
+                m_userService.findByExperienceYears(minExperience, maxExperience, page, size));
+        model.addAttribute("minExperience", minExperience);
+        model.addAttribute("maxExperience", maxExperience);
+        return "user/experienceBetween";
+    }
+
+    @GetMapping("/all")
+    public String showAllUsersPage(@RequestParam(defaultValue = "0") int page,
+                                   @RequestParam(defaultValue = "5") int size,
+                                   Model model)
+    {
+        model.addAttribute("users", m_userService.findAll(page, size));
+        return "user/allUsers";
+    }
+
+    @GetMapping("/all/company/{companyId}")
+    public String showAllUsersByCompany(@PathVariable long companyId,
+                                        @RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "5") int size,
+                                        Model model)
+    {
+        try {
+            var users = m_userService.findAllUserByCompany(companyId, page, size);
+            model.addAttribute("users", users);
+            return "user/allUsers";
+        }
+        catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return "error/errorPage";
+        }
+
+    }
+
+    @GetMapping("/search/{keyword}")
+    public String searchUsers(@PathVariable String keyword,
+                              @RequestParam(defaultValue = "0") int page,
+                              @RequestParam(defaultValue = "5") int size,
+                              Model model)
+    {
+        var users = m_userService.searchUsers(keyword, page, size);
+
+        if (users.getTotalElements() == 0)
+            model.addAttribute("errorMessage", "No user found with the entered qualifications.");
+
+        model.addAttribute("users", users);
+        model.addAttribute("keyword", keyword);
+        return "user/searchUsers";
+    }
+
+    @GetMapping("/job/posting")
+    public String showJobPostingPage(@RequestParam long userId, Model model)
+    {
+        try {
+            var jobs = m_userService.getRecommendedJobs(userId);
+
+            if (jobs.isEmpty()) {
+                model.addAttribute("errorMessage", "No jobs found");
+                return "jobPosting/jobPosting";
+            }
+
+            model.addAttribute("jobPostingDTO", jobs);
+            return "jobPosting/jobPosting";
+        }
+        catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return "error/errorPage";
+        }
+    }
+
+}
