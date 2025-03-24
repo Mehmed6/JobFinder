e204262c078fc6c847284b106fbc29ac2d3ad0b9
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-03-24 23:46:08 +0100

    Update
    Dashboard controller and form updated

--- JobFinder-v1.0/src/main/java/com/doganmehmet/app/controller/DashboardController.java
+++ JobFinder-v1.0/src/main/java/com/doganmehmet/app/controller/DashboardController.java
@@ -1,18 +1,37 @@
 package com.doganmehmet.app.controller;
 
+import com.doganmehmet.app.exception.ApiException;
+import com.doganmehmet.app.exception.MyError;
+import com.doganmehmet.app.repository.IUserRepository;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
-    @GetMapping
-    public String dashboard()
+    private final IUserRepository m_userRepository;
+
+    public DashboardController(IUserRepository userRepository)
     {
+        m_userRepository = userRepository;
+    }
+
+    @GetMapping
+    public String dashboard(Model model)
+    {
+        var email = SecurityContextHolder.getContext().getAuthentication().getName();
+        var user = m_userRepository.findByEmail(email)
+                .orElseThrow(() -> new ApiException(MyError.USER_NOT_FOUND));
+        var fullName = user.getFirstname() + " " + user.getLastname();
+        model.addAttribute("fullName", fullName.toUpperCase());
+        model.addAttribute("userId", user.getUserId());
+
         return "dashboard/dashboard";
     }
 }
