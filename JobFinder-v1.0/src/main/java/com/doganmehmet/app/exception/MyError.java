aa57c653c46d4b759ed552c7fa1d46c3c7a628d3
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-03-24 23:48:59 +0100

    Update MyError.java

--- JobFinder-v1.0/src/main/java/com/doganmehmet/app/exception/MyError.java
+++ JobFinder-v1.0/src/main/java/com/doganmehmet/app/exception/MyError.java
@@ -21,6 +21,12 @@ public enum MyError {
     JOB_ALREADY_APPLIED("1009", "You have already applied for this job!"),
     COMPANY_ALREADY_EXISTS("1010", "Company already exists!"),
     INVALID_STATUS("1011", "Invalid status!"),
+    JOB_APPLICATION_NOT_FOUND("1012", "Job Application not found!"),
+    JOB_APPLICATION_ALREADY_APPROVED("1013", "This job application has already been approved!"),
+    JOB_APPLICATION_ALREADY_REJECTED("1014", "This job application has already been rejected!"),
+    JOB_POSTING_INACTIVE("1015", "This job posting is inactive or has been closed!"),
+    USER_CANNOT_APPLY_FOR_ANOTHER_USER("1016", "User cannot apply for another user!"),
+    WRITE_TO_FILE_EXCEPTION("1017", "An error occurred while writing the file!"),
 
     ;
 
