c6c16ce10d10a8bb53bd9571a69b05c5b680ba0d
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-03-29 04:52:59 +0100

    Add admin dashboard
    Added a dashboard accessible only to admins.
    Enabled adding new companies and posting job listings.
    Allowed viewing all job applications.

--- JobFinder-v1.0/src/main/java/com/doganmehmet/app/controller/DashboardController.java
+++ JobFinder-v1.0/src/main/java/com/doganmehmet/app/controller/DashboardController.java
@@ -1,14 +1,13 @@
 package com.doganmehmet.app.controller;
 
+import com.doganmehmet.app.enums.Role;
 import com.doganmehmet.app.exception.ApiException;
 import com.doganmehmet.app.exception.MyError;
 import com.doganmehmet.app.repository.IUserRepository;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,8 +27,14 @@ public class DashboardController {
         var email = SecurityContextHolder.getContext().getAuthentication().getName();
         var user = m_userRepository.findByEmail(email)
                 .orElseThrow(() -> new ApiException(MyError.USER_NOT_FOUND));
+
         var fullName = user.getFirstname() + " " + user.getLastname();
         model.addAttribute("fullName", fullName.toUpperCase());
+
+        if (user.getRole() == Role.ADMIN) {
+            return "dashboard/adminDashboard";
+        }
+
         model.addAttribute("userId", user.getUserId());
 
         return "dashboard/dashboard";
